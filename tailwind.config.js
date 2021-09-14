b50d535d7566e89a4ed82160646145a4337ea389
evondev <trananhtuan400@gmail.com>
2021-09-14 23:02:21 +0700

    Add darkmode tutorial part 5

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   mode: "jit",
   purge: ["./**/*.html", "./**/*.{js,jsx,ts,tsx,vue}"],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: "class", // or 'media' or 'class'
   theme: {
     extend: {
       spacing: {
